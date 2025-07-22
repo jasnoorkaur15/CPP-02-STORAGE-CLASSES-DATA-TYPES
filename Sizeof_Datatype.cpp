c9a0971e0d8e3ad0f2666ac22f3823750d7d9420
jasnoorkaur15 <kjasnoor15@gmail.com>
2025-07-23 00:15:28 +0530

    Create Sizeof_Datatype.cpp

---
+++ Sizeof_Datatype.cpp
@@ -0,0 +1,18 @@
+//Jasnoor Kaur
+//24070123049
+//Batch: A2
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Size of various datatypes"<<endl;
+    cout<<"Integer: "<<sizeof(int)<<endl;
+    cout<<"Float: "<<sizeof(float)<<endl;
+    cout<<"Boolean: "<<sizeof(bool)<<endl;
+    cout<<"Long Integer: "<<sizeof(long int)<<endl;
+    cout<<"Short Integer: "<<sizeof(short int)<<endl;
+    cout<<"Double: "<<sizeof(double)<<endl;
+    cout<<"Character: "<<sizeof(char)<<endl;
+    return 0;
+}
